a3dbacdfa23a4ac244bdd69b2aa9913d97c603f6
Irma Mejia <ijanettmejia@outlook.com>
2019-09-13 02:37:17 -0400

    Add index action to subcategories controller

--- app/controllers/subcategories_controller.rb
+++ app/controllers/subcategories_controller.rb
@@ -1,2 +1,6 @@
 class SubcategoriesController < ApplicationController
+    def index
+        subcategories = Subcategory.all
+        render json: SubcategorySerializer.new(subcategories)
+    end
 end
