4998e6682478b2357ab49d17c7440f82a34904ee
Irma Mejia <ijanettmejia@outlook.com>
2019-09-13 00:38:47 -0400

    Add show action to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,14 @@
 class UsersController < ApplicationController
+    # def index
+    #     users = User.all
+    #     render json: UserSerializer.new(users)
+    # end
+
+    def show
+        user = User.find_by(id: params[:id])
+        options = {
+            include: [:budgets]
+        }
+        render json: UserSerializer.new(user, options)
+    end
 end
