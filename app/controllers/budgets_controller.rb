2de43c256aa35edc2d72c956c652e18d49f01220
Irma Mejia <ijanettmejia@outlook.com>
2019-09-13 00:37:14 -0400

    Add create action to budgets controller

--- app/controllers/budgets_controller.rb
+++ app/controllers/budgets_controller.rb
@@ -1,2 +1,18 @@
 class BudgetsController < ApplicationController
+
+    # def index
+    #     budgets = Budget.all
+    #     render json: BudgetSerializer.new(budgets)
+    # end
+
+    def create
+        budget = Budget.create(budget_params)
+        render json: BudgetSerializer.new(budget)
+    end
+
+    private
+    
+    def budget_params
+        params.require(:budget).permit(:amount, :category, :description, :user_id, :subcategory_id)
+    end
 end
