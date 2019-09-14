b9eb1b5e5a776ec72274adbbe0119f6957bdd2e3
Irma Mejia <ijanettmejia@outlook.com>
2019-09-14 10:45:36 -0400

    Add description attribute to budget serializer

--- app/serializers/budget_serializer.rb
+++ app/serializers/budget_serializer.rb
@@ -2,5 +2,5 @@ class BudgetSerializer
   include FastJsonapi::ObjectSerializer
   belongs_to :user
   belongs_to :subcategory
-  attributes :amount, :category, :user, :subcategory
+  attributes :amount, :category, :description, :user, :subcategory
 end
